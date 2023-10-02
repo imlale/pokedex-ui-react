f2ff96cd3404d754dcdcb9b85dcf15f37410a026
imlale <laleeslale123@gmail.com>
2023-10-02 16:03:07 -0500

    Added Preloaded Boxes

--- src/components/PokemonList/index.tsx
+++ src/components/PokemonList/index.tsx
@@ -9,7 +9,7 @@ import { useTranslation } from "react-i18next";
 import { fetchPokemonList, fetchPokemonListDetail } from "../../services/fetchPokemonApi";
 
 
-
+const CANTIDAD_REGISTROS = 14
 //funcion para hacer un string de tamaño 5, en este caso 00001 a 00015
 function numberToPokeString(number: number) {
   return "#" + ("000" + number).slice(-4);
@@ -28,7 +28,7 @@ const PokemonList: React.FC = () => {
       fetchPokemonList("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1292")
         .then(results => {
           setPokemonFetchedList(results);
-          fetchPokemonListDetail(pokemonFetchedList, 0, 14);
+          fetchPokemonListDetail(pokemonFetchedList, 0, CANTIDAD_REGISTROS);
         })
 
     } catch (error: any) {
@@ -40,7 +40,7 @@ const PokemonList: React.FC = () => {
 
     setIsLoading(true);
 
-    fetchPokemonListDetail(pokemonFetchedList, siguientePagina, 14)
+    fetchPokemonListDetail(pokemonFetchedList, siguientePagina, CANTIDAD_REGISTROS)
       .then((updatedPokemonList) => {
         setIsLoading(false);
         if (siguientePagina === 0) {
@@ -52,21 +52,23 @@ const PokemonList: React.FC = () => {
   }, [pokemonFetchedList, siguientePagina])
 
   const handleCargarMas = () => {
-    if(pokemonFetchedList.length === 0) {
+    if (pokemonFetchedList.length === 0) {
       return;
     }
-    setSiguientePagina(siguientePagina+14);
+    setSiguientePagina(siguientePagina + CANTIDAD_REGISTROS);
   }
 
-  
+
   return (
     <div>
 
       <div className="row pokemon-list">
+        
         {pokemonList.map((pokemon: any, index: number) => {
           return <PokemonCard key={pokemon.id} pokemon={pokemon} isLoading={isLoading && index >= siguientePagina}
             onSelectedPokemonChange={handleSelectedPokemonChange} />
         })}
+        {isLoading? <PokemonCardDummy pokemonList={pokemonFetchedList.slice(siguientePagina, siguientePagina + CANTIDAD_REGISTROS)}/>: ""}
         <div className="cargar-mas">
 
           <FinDePaginaObserver isLoading={isLoading} onFinDePagina={() => handleCargarMas()} />
@@ -84,18 +86,18 @@ const PokemonCard = ({ pokemon, onSelectedPokemonChange, isLoading }: { pokemon:
     data-bs-toggle="offcanvas"
     data-bs-target={"#offcanvasRight"} aria-controls="offcanvasRight"
     onClick={() => onSelectedPokemonChange(pokemon.id)}>
-    <div className={`card custom-card ${pokemon.types[0].type.name}`} >
+    <div className={`card custom-card ${pokemon.types[0]?.type.name}`} >
       <div className="row">
         <div className="col no-wrap">
           <h6>{numberToPokeString(pokemon.id)}</h6>
           <h5 className="is-title">{pokemon.name}</h5>
           <div className="types">
-            {pokemon.types.map((type: any, index: number) => {
+            {pokemon.types?.map((type: any, index: number) => {
               return <TypeBadge key={index} name={type.type.name} />
             })}
           </div>
           <div className={`poke-image ${pokemon.sprite ? "" : "no-image"} ${imageLoading ? "loading" : ""}`}>
-            {(imageLoading)? <div className="position-absolute"><Loader /></div> : ""}
+            {(imageLoading) ? <div className="position-absolute"><Loader /></div> : ""}
             {(isLoading) ? <Loader /> : pokemon.sprite ? <img onLoad={() => setImageLoading(false)} src={pokemon.sprite} alt={pokemon.name} className="w-100" /> : ""}
           </div>
         </div>
@@ -111,6 +113,26 @@ export const TypeBadge = ({ name }: { name: string }) => {
     <span className="icon"></span>
     <span className="is-title">{t(`types.${name ? name : "unknown"}`)}</span>
   </div>
+}
+
+function PokemonCardDummy( {pokemonList}: { pokemonList: { name: string, url: string}[] }) {
+  let pokeItem = {} as Pokemon;
+  pokeItem = {
+    name: "name",
+    url: "url",
+    types: [],
+    id: 0,
+    sprite: "sprite"
+  }
+ 
+  const array = pokemonList.map((pokemon)=>{
+    return {...pokeItem, name: pokemon.name}
+  });
+  
+  return array.map((pokemon, index)=>{
+    return <PokemonCard key={index} pokemon={pokemon} isLoading={true}
+    onSelectedPokemonChange={()=>{}} />
+  })
 }
 
 
